ef7bed5c6665d3d94e336a544dabb557be89130d
andrey_lutsevich <andrey-360.90@mail.ru>
2024-01-21 14:18:17 +0300

    fix: исправляет баг шапки
    - при маленькой высоте меню не скроллось;
    - при переходе из мобильного меню на десктоп не снималась блокировка скролла страницы

--- source/js/main.js
+++ source/js/main.js
@@ -32,6 +32,8 @@ window.addEventListener('DOMContentLoaded', () => {
   // все скрипты должны быть в обработчике 'DOMContentLoaded', но не все в 'load'
   // в load следует добавить скрипты, не участвующие в работе первого экрана
   window.addEventListener('load', () => {
+    const burger = new Burger();
+    burger.init();
     uploadFile();
     uploadImageDrop();
     const select = new CustomSelect();
@@ -39,8 +41,6 @@ window.addEventListener('DOMContentLoaded', () => {
     const form = new Form();
     window.form = form;
     form.init();
-    const burger = new Burger();
-    burger.init();
     const stickyHeader = new StickyHeader();
     stickyHeader.init();
     initAccordions();
--- source/js/modules/burger/burger.js
+++ source/js/modules/burger/burger.js
@@ -1,10 +1,11 @@
-import scrollLock from './scroll-lock';
+import scrollLock from '../../vendor/scroll-lock.min';
 import {FocusLock} from '../../utils/focus-lock';
 
 export class Burger {
   constructor() {
     this._header = document.querySelector('[data-header]');
     this._burger = document.querySelector('[data-burger]');
+    this._scrollableElement = document.querySelector('.header__menu-inner');
     this._scrollLock = scrollLock;
     this._focusLock = new FocusLock();
     this._isMenuOpen = false;
@@ -12,6 +13,7 @@ export class Burger {
     this._onBurgerClick = this._onBurgerClick.bind(this);
     this._onDocumentKeydown = this._onDocumentKeydown.bind(this);
     this._onDocumentClick = this._onDocumentClick.bind(this);
+    this._closeMenuResize = this._closeMenuResize.bind(this);
   }
 
   init() {
@@ -20,14 +22,16 @@ export class Burger {
     }
 
     this._burger.addEventListener('click', this._onBurgerClick);
+
   }
 
   _openMenu() {
     this._isMenuOpen = true;
     this._header.classList.add('is-open');
-    this._scrollLock.disablePageScroll();
+    this._scrollLock.disablePageScroll(this._scrollableElement);
     document.addEventListener('keydown', this._onDocumentKeydown);
     document.addEventListener('click', this._onDocumentClick);
+    window.addEventListener('resize', this._closeMenuResize);
     this._focusLock.lock('[data-header]');
     if (window.ls) {
       window.ls.stop();
@@ -37,12 +41,19 @@ export class Burger {
   _closeMenu() {
     this._isMenuOpen = false;
     this._header.classList.remove('is-open');
-    this._scrollLock.enablePageScroll();
+    this._scrollLock.enablePageScroll(this._scrollableElement);
     this._focusLock.unlock('[data-header]');
     document.removeEventListener('keydown', this._onDocumentKeydown);
     document.removeEventListener('click', this._onDocumentClick);
+    window.removeEventListener('resize', this._closeMenuResize);
     if (window.ls) {
       window.ls.start();
+    }
+  }
+
+  _closeMenuResize() {
+    if (window.screen.width > 1023) {
+      this._closeMenu();
     }
   }
 
