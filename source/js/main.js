50f4e8c6aa62b4480b20f6c74c21bed58a92cc15
andrey_lutsevich <andrey-360.90@mail.ru>
2024-03-21 23:09:00 +0300

    fix: убирает ненужный код

--- source/js/main.js
+++ source/js/main.js
@@ -33,8 +33,6 @@ window.addEventListener('DOMContentLoaded', () => {
   initScrollTrigger();
   initScrollHeaderChange();
 
-  // все скрипты должны быть в обработчике 'DOMContentLoaded', но не все в 'load'
-  // в load следует добавить скрипты, не участвующие в работе первого экрана
   window.addEventListener('load', () => {
     uploadFile();
     uploadImageDrop();
@@ -49,28 +47,3 @@ window.addEventListener('DOMContentLoaded', () => {
     showMessage();
   });
 });
-
-// ---------------------------------
-
-// ❗❗❗ обязательно установите плагины eslint, stylelint, editorconfig в редактор кода.
-
-// привязывайте js не на классы, а на дата атрибуты (data-validate)
-
-// вместо модификаторов .block--active используем утилитарные классы
-// .is-active || .is-open || .is-invalid и прочие (обязателен нейминг в два слова)
-// .select.select--opened ❌ ---> [data-select].is-open ✅
-
-// выносим все в дата атрибуты
-// url до иконок пинов карты, настройки автопрокрутки слайдера, url к json и т.д.
-
-// для адаптивного JS используейтся matchMedia и addListener
-// const breakpoint = window.matchMedia(`(min-width:1024px)`);
-// const breakpointChecker = () => {
-//   if (breakpoint.matches) {
-//   } else {
-//   }
-// };
-// breakpoint.addListener(breakpointChecker);
-// breakpointChecker();
-
-// используйте .closest(el)
